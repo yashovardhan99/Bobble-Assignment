7eafb76696d8969f705fb7c6bb129d4d36c998c6
yashovardhan99 <yashovardhandhanania@gmail.com>
2020-09-06 13:50:46 +0530

    Added animations for edit text

--- BobbleBigText/app/src/main/java/com/yashovardhan99/bobblebigtext/EditBar.kt
+++ BobbleBigText/app/src/main/java/com/yashovardhan99/bobblebigtext/EditBar.kt
@@ -1,11 +1,10 @@
 package com.yashovardhan99.bobblebigtext
 
-import androidx.compose.foundation.BaseTextField
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.Icon
-import androidx.compose.foundation.clickable
+import androidx.compose.animation.animatedFloat
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.ColumnScope.gravity
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Send
 import androidx.compose.runtime.*
@@ -14,10 +13,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.gesture.LongPressDragObserver
 import androidx.compose.ui.gesture.longPressDragGestureFilter
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.coerceAtMost
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.ui.tooling.preview.Preview
@@ -61,11 +60,14 @@ fun SendButton(
 @ExperimentalFoundationApi
 @Composable
 fun BigTextField(size: TextUnit, modifier: Modifier = Modifier) {
+    val scrollState = rememberScrollState()
     var value by remember { mutableStateOf(TextFieldValue("")) }
     BaseTextField(
         value = value, onValueChange = { updated ->
             value = updated
-        }, textStyle = TextStyle(fontSize = size), modifier = modifier
+        }, textStyle = TextStyle(fontSize = size), modifier = modifier.verticalScroll(scrollState)
+            .background(MaterialTheme.colors.surface, RectangleShape)
+            .padding(4.dp)
     )
 }
 
@@ -73,12 +75,13 @@ fun BigTextField(size: TextUnit, modifier: Modifier = Modifier) {
 @Composable
 fun SendBar(modifier: Modifier = Modifier) {
     var text by remember { mutableStateOf(false) }
-    var size by remember { mutableStateOf(12.sp) }
     val scope = rememberCoroutineScope()
+    val size = animatedFloat(initVal = 12f)
+    size.setBounds(12f, 100f)
     ConstraintLayout(modifier = modifier.fillMaxWidth()) {
         val edit = createRef()
         val button = createRef()
-        BigTextField(size = size, modifier = Modifier.constrainAs(edit) {
+        BigTextField(size = size.value.sp, modifier = Modifier.constrainAs(edit) {
             start.linkTo(parent.start)
             top.linkTo(parent.top)
             end.linkTo(button.start)
@@ -86,19 +89,20 @@ fun SendBar(modifier: Modifier = Modifier) {
             height = Dimension.preferredWrapContent.atLeast(24.dp)
         }.gravity(Alignment.Start))
         SendButton(onClick = {
-            size = 12.sp
+            size.animateTo(12f)
         }, onLongStart = {
             text = true
             scope.launch {
                 while (text) {
-                    size = (size + 0.2.sp).coerceAtMost(100.sp)
-                    delay(20)
+                    size.animateTo(size.value + 1f)
+                    delay(100)
                 }
             }
         }, onLongEnd = {
             text = false
         }, modifier = Modifier.constrainAs(button) {
             end.linkTo(parent.end)
+            bottom.linkTo(parent.bottom)
         })
     }
 }
--- BobbleBigText/app/src/main/java/com/yashovardhan99/bobblebigtext/MainActivity.kt
+++ BobbleBigText/app/src/main/java/com/yashovardhan99/bobblebigtext/MainActivity.kt
@@ -2,16 +2,26 @@ package com.yashovardhan99.bobblebigtext
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.ConstraintLayout
+import androidx.compose.foundation.layout.Dimension
+import androidx.compose.foundation.layout.RowScope.gravity
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.unit.dp
+import androidx.ui.tooling.preview.Preview
+import androidx.ui.tooling.preview.PreviewParameter
 import com.yashovardhan99.bobblebigtext.ui.BobbleBigTextTheme
 
+@ExperimentalFoundationApi
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,10 +34,49 @@ class MainActivity : AppCompatActivity() {
                             Text(text = "BigText")
                         })
                     }) {
-                        ConversationList(list = getFakeMessages(), modifier = Modifier.padding(it))
+                        MainPage(list = getFakeMessages(), modifier = Modifier.padding(it))
                     }
                 }
             }
         }
     }
+}
+
+@ExperimentalFoundationApi
+@Composable
+fun MainPage(modifier: Modifier = Modifier, list: List<Message>) {
+    ConstraintLayout(modifier.gravity(Alignment.Bottom)) {
+        val convList = createRef()
+        val sendBar = createRef()
+        ConversationList(list = list,
+            modifier = Modifier.constrainAs(convList) {
+                top.linkTo(parent.top)
+                bottom.linkTo(sendBar.top)
+                width = Dimension.fillToConstraints
+                height = Dimension.preferredWrapContent
+            })
+        SendBar(modifier = Modifier.constrainAs(sendBar) {
+            bottom.linkTo(parent.bottom)
+            width = Dimension.fillToConstraints
+            height = Dimension.preferredValue(50.dp)
+        })
+    }
+}
+
+@ExperimentalFoundationApi
+@Preview
+@Composable
+fun MainPagePreview(@PreviewParameter(BooleanPreviewProvider::class) isDark: Boolean) {
+    BobbleBigTextTheme(isDark) {
+        Surface() {
+            Scaffold(topBar = {
+                TopAppBar(title = {
+                    Text(text = "BigText")
+                })
+            }) {
+                MainPage(list = getFakeMessages(), modifier = Modifier.padding(it))
+            }
+        }
+    }
+
 }
